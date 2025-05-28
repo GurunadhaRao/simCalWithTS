1479d78f0d43e2e1e5f2a6802bb0e4309aaed826
GurunadhD <gurunadhacc@gmail.com>
2025-05-28 20:16:21 +0530

    Working simple calculator with TypeScript

--- calculator.js
+++ calculator.js
@@ -1,24 +1,8 @@
-type InvestmentData = {
-    initialAmount: number;
-    annualContrib: number;
-    expectedReturn: number;
-    duration: number;
-};
-
-type InvestmentResult = {
-    year: string;
-    totalAmount: number;
-    totalContributions: number;
-    totalInterestEarned: number;
-}
-
-type CalculationResult = InvestmentResult[] | string;
-
-function calculateInvestment(data: InvestmentData): CalculationResult {
+"use strict";
+function calculateInvestment(data) {
     const { initialAmount, annualContrib, expectedReturn, duration } = data;
-
     if (initialAmount < 0) {
-        return 'Initial investment amount must be atleast zero.'
+        return 'Initial investment amount must be atleast zero.';
     }
     if (duration <= 0) {
         return 'No valid amount of years provided.';
@@ -26,52 +10,42 @@ function calculateInvestment(data: InvestmentData): CalculationResult {
     if (expectedReturn < 0) {
         return 'Expected return must be atleast zero.';
     }
-
     let total = initialAmount;
     let totalContributions = 0;
     let totalInterestEarned = 0;
-
-    const annualResults: InvestmentResult[] = [];
-
+    const annualResults = [];
     for (let i = 0; i < duration; i++) {
         total = total * (1 + expectedReturn);
         totalInterestEarned = total - totalContributions - initialAmount;
         totalContributions = totalContributions + annualContrib;
         total = total + annualContrib;
-
         annualResults.push({
             year: `Year ${i + 1}`,
             totalAmount: total,
             totalContributions,
             totalInterestEarned
-        })
+        });
     }
-
     return annualResults;
 }
-
-function printResults(results: CalculationResult) {
+function printResults(results) {
     if (typeof results === 'string') {
         console.log(results);
         return;
     }
-
     for (const yearEndResult of results) {
         console.log(yearEndResult.year);
         console.log(`Total: ${yearEndResult.totalAmount.toFixed(0)}`);
         console.log(`Total Contributions: ${yearEndResult.totalContributions.toFixed(0)}`);
         console.log(`Total Interest Earned: ${yearEndResult.totalInterestEarned.toFixed(0)}`);
-        console.log(`----------------------------`)
+        console.log(`----------------------------`);
     }
 }
-
-const investmentData: InvestmentData = {
+const investmentData = {
     initialAmount: 5000,
     annualContrib: 500,
     expectedReturn: 0.08,
     duration: 10
 };
-
 const results = calculateInvestment(investmentData);
-
 printResults(results);
