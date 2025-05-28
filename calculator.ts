c7631b8d6bae010fff3a96e6c3049aad2d976e28
GurunadhD <gurunadhacc@gmail.com>
2025-05-28 20:03:02 +0530

    completed calculateInvestment functionality.

--- calculator.ts
+++ calculator.ts
@@ -14,7 +14,41 @@ type InvestmentResult = {
 
 type CalculationResult = InvestmentResult[] | string;
 
-function calculateInvestment(data: InvestmentData): CalculationResult {} 
+function calculateInvestment(data: InvestmentData): CalculationResult {
+    const { initialAmount, annualContrib, expectedReturn, duration } = data;
+
+    if (initialAmount < 0) {
+        return 'Initial investment amount must be atleast zero.'
+    }
+    if (duration <= 0) {
+        return 'No valid amount of years provided.';
+    }
+    if (expectedReturn < 0) {
+        return 'Expected return must be atleast zero.';
+    }
+
+    let total = initialAmount;
+    let totalContributions = 0;
+    let totalInterestEarned = 0;
+
+    const annualResults: InvestmentResult[] = [];
+
+    for (let i = 0; i < duration; i++) {
+        total = total * (1 + expectedReturn);
+        totalInterestEarned = total - totalContributions - initialAmount;
+        totalContributions = totalContributions + annualContrib;
+        total = total + annualContrib;
+        
+        annualResults.push({
+            year: `Year ${i + 1}`,
+            totalAmount: total,
+            totalContributions,
+            totalInterestEarned
+        })
+    }
+
+    return annualResults;
+} 
 
 function printResults(results) {}
 
